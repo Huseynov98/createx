aa5a02ba3cbbe7f4012585eb604602a9e44fa36c
Huseynov Rustam <122001750+Huseynov98@users.noreply.github.com>
2023-05-07 14:21:14 +0300

    Delete JS directory

--- JS/index.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    var mixer = mixitup('.directions__list');
-    $('.directions__filter-btn').on('click', function(e) {
-        $(this).addClass('directions__filter-btn--active');
-    })
-})
